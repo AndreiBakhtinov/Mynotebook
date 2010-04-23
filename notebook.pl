691bb193ade301b1b24818b2811e298e6874dcae
Xspeed <xspeed.id@gmail.com>
2010-04-23 12:32:09 +0400

    NOTEBOOK-PROJECT

---
+++ notebook.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl -w
+## Mynotebook ver.1
+## by Xspeed, 2010.
+
+use Fcntl;
+my $DBfile = $ARGV[0] || './mynotes.txt';
+if (-e $DBfile) { die qq('$DBfile' is not a text file. Quit.\n) unless -T $DBfile; }
+
+my %Commands = qw(	add a
+					find f
+					show s
+					remove r
+				);
+my $Notes;
+sysopen(DB, $DBfile, O_RDWR|O_CREAT, 0600) || die "Can't open DB: $!\n";
+$Notes = () = <DB>;
+close DB;
+
+print "-= My notebook =-\n";
+print '* ',`date`;
+print '* Editing: ', "$DBfile\n";
+print '* Notes: ', "$Notes\n";
+print CommandsList();
+
+while (($_=<STDIN>) !~ /^\s*$/) {
+	chomp;
+	if (/^$Commands{'add'}\s+?(.+)$/) {
+		open (DB, '>>', $DBfile) || die "Can't open DB: $!\n";
+		print DB "$1\n";
+		close DB;
+	}
+	elsif (/^$Commands{'find'}\s+?(.+)$/) {
+		my $term = $1;
+		open (DB, $DBfile) || die "Can't open DB: $!\n";
+		while (<DB>) {
+			print ":$. $_" if m/$term/i;
+		}
+		close DB;
+	}
+	elsif (/^$Commands{'show'}\s*$/) {
+		open (DB, $DBfile) || die "Can't open DB: $!\n";
+		print "$. $_" while <DB>;
+		close DB;
+	}
+	elsif (/^$Commands{'remove'}\s+?(\d+)$/) {
+		my $n = $1;
+		open (DB, $DBfile) || die "Can't open DB: $!\n";
+		my @tmp = <DB>;
+		close DB;
+		open (DB, '>', $DBfile) || die "Can't open DB: $!\n";
+		delete $tmp[$n-1] || print "(!) Wrong note number: # ${n}\n";
+		for (@tmp) { print DB if defined; }
+		close DB;
+		undef @tmp;
+	}
+	else {
+		print "(!) Wrong command: $_\n";
+	}
+	print CommandsList();
+}
+
+sub CommandsList {
+	my $output;
+	while(my($a,$b) = each %Commands) {
+		 $output .= "$a => $b\; ";
+	}
+	substr($output,-2) = ' ';
+	return "$output: ";
+}
